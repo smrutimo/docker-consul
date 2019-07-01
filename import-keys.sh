3348d154502e422c29e7e70a5bf19354618973d2
Smruti Mohapatra <smruti.mohapatra@gmail.com>
2019-07-01 12:26:56 -0400

    fix script

--- import-keys.sh
+++ import-keys.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 echo "importing keys after startup"
+yq
 for f in /kv-init/*.json
 do
   echo "Importing key-values from file: $f"
-  jq 'map(.key as $k | .flags as $f | .value | @base64 as $v | {key: $k, flags: $f, value: $v})' < $f | consul kv import -
+  jq 'map(.key as $k | .flags as $f | .value | @base64 as $v | {key: $k, flags: $f, value: $v})' $f | consul kv import -
 done
 echo "done importing keys"
